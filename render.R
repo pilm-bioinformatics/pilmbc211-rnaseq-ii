803380fcc28b500286462c8c98d2426989c72560
Lorena Pantano <lorena.pantano@gmail.com>
2019-07-05 14:57:35 -0400

    update Rmd code

--- render.R
+++ render.R
@@ -1,5 +1,6 @@
 rmarkdown::render("load.Rmd", output_file = "docs/load_data.html")
 
 rmarkdown::render("de.Rmd", output_file = "docs/differential_expression_analysis.html")
-rmarkdown::render("rmarkdown.md", output_file = "docs/rmarkdown.html")
+rm("params")
+rmarkdown::render("run_render.Rmd", output_file = "docs/run_render.html")
 rmarkdown::render("readme.md", output_file = "docs/index.html")
